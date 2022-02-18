d7876a0c031e3359327eca15b4165dc6bb6e80a2
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-02-18 11:13:27 +0200

    Cleanup code that duplicates Magento logic since 2.3.0. See Magento/Catalog/Model/Category/DataProvider::convertValues

--- Plugin/Model/Category/DataProvider.php
+++ Plugin/Model/Category/DataProvider.php
@@ -7,28 +7,16 @@ use Magento\Framework\App\ObjectManager;
 class DataProvider
 {
     /**
-     * @var \Swissup\Easycatalogimg\Helper\Image
-     */
-    protected $helper;
-
-    /**
      * @var \Magento\Framework\UrlInterface
      */
     protected $urlBuilder;
 
     /**
-     * @var \Magento\Catalog\Model\Category\FileInfo|null
-     */
-    protected $fileInfo;
-
-    /**
-     * @param \Swissup\Easycatalogimg\Helper\Image $helper
+     * @param \Magento\Framework\UrlInterface $urlBuilder
      */
     public function __construct(
-        \Swissup\Easycatalogimg\Helper\Image $helper,
         \Magento\Framework\UrlInterface $urlBuilder
     ) {
-        $this->helper = $helper;
         $this->urlBuilder = $urlBuilder;
     }
 
@@ -68,24 +56,6 @@ class DataProvider
         $category = $subject->getCurrentCategory();
         if ($category) {
             $id = $category->getId();
-            if (isset($result[$id]['thumbnail'])) {
-                unset($result[$id]['thumbnail']);
-                $thumbnail = $category->getData('thumbnail');
-                $url = false;
-                if ($thumbnail && is_string($thumbnail)) {
-                    $thumbnail = $this->helper->prepareThumbnailUrl($thumbnail);
-                    $url = $this->helper->getBaseUrl() . $thumbnail;
-                }
-
-                $result[$id]['thumbnail'][0]['name'] = $thumbnail;
-                $result[$id]['thumbnail'][0]['url'] = $url;
-                if (null !== $this->getFileInfo()) {
-                    $stat = $this->getFileInfo()->getStat($thumbnail);
-                    $mime = $this->getFileInfo()->getMimeType($thumbnail);
-                    $result[$id]['thumbnail'][0]['size'] = isset($stat) ? $stat['size'] : 0;
-                    $result[$id]['thumbnail'][0]['type'] = $mime;
-                }
-            }
 
             // unset image data if there are no name and url
             if (isset($result[$id]['image'])) {
@@ -98,23 +68,5 @@ class DataProvider
         }
 
         return $result;
-    }
-
-    /**
-     * Get FileInfo instance
-     *
-     * @return null|\Magento\Catalog\Model\Category\FileInfo
-     */
-    private function getFileInfo()
-    {
-        if ($this->fileInfo === null
-            && class_exists(\Magento\Catalog\Model\Category\FileInfo::class)
-        ) {
-            // Magento less than 2.2.0 does not have class FileInfo
-            // that is why we have to use object manager
-            $this->fileInfo = ObjectManager::getInstance()
-                ->get(\Magento\Catalog\Model\Category\FileInfo::class);
-        }
-        return $this->fileInfo;
     }
 }
