9cf04bb15d501dc85bb56bb5cb185169148bc78e
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-04-07 12:00:37 +0300

    MCS: use data patch instead of setup scripts

---
+++ Setup/Patch/Data/CreateThumbnailAttribute.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Swissup\Easycatalogimg\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Category;
+use Magento\Catalog\Model\Category\Attribute\Backend\Image;
+use Magento\Catalog\Setup\CategorySetupFactory;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+class CreateThumbnailAttribute implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var CategorySetupFactory
+     */
+    private $categorySetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CategorySetupFactory $categorySetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->categorySetupFactory = $categorySetupFactory;
+    }
+
+    public function apply()
+    {
+        $this->categorySetupFactory
+            ->create(['setup' => $this->moduleDataSetup])
+            ->addAttribute(Category::ENTITY, 'thumbnail', [
+                'type' => 'varchar',
+                'label' => 'Thumbnail',
+                'input' => 'image',
+                'backend' => Image::class,
+                'required' => false,
+                'sort_order' => 6,
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'group' => 'General Information',
+            ]);
+    }
+
+    public static function getVersion()
+    {
+        return '1.0.2';
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
