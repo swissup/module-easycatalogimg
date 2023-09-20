533dde046ef038669b2ff0cce4a2b6e4ae704e9e
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2023-09-20 16:13:28 +0300

    Integration of 'hide_when_filter_is_used' with Swissup_AjaxLayerednavigation module Closes #12

---
+++ view/frontend/web/js/easycatalogimg.js
@@ -0,0 +1,35 @@
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    $.widget('swissup.easycatalogimg', {
+        component: 'Swissup_Easycatalogimg/js/easycatalogimg',
+
+        _create: function () {
+            if (this.options.hide_when_filter_is_used) {
+                this.hideAfterAjaxFilters();
+            }
+        },
+
+        hideAfterAjaxFilters: function () {
+            $(document).on('swissup:ajaxlayerednavigation:reload:after', (event, data) => {
+                setTimeout(() => {
+                    var applyMultiple = $('#layered-filter-apply-tooltip');
+
+                    if (applyMultiple.length && applyMultiple[0].style.left !== '-1000px') {
+                        return;
+                    }
+
+                    if (data.response.state || $('[data-page-number]').last().data('page-number') > 1) {
+                        this.element.hide();
+                    } else {
+                        this.element.show();
+                    }
+                }, 20);
+            });
+        }
+    });
+
+    return $.swissup.easycatalogimg;
+});
